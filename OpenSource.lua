c627c5d21772a5690eb9338dbc2cb258efca7b2c
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-12-22 12:09:02 -0500

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -19,131 +19,10 @@ local Tab1 = Window:CreateTab("Main")
 
 local Section1 = Tab1:CreateSection("Main Section")
 
-Section1:CreateToggle("Unit Esp | Kinda Laggy",nil,function(value)
-    tog = value
-    local runServ = game:GetService("RunService")
-if tog == true then
-runServ:BindToRenderStep("test", Enum.RenderPriority.Camera.Value, function() 
-for i,v in pairs(game:GetService("Workspace").Units:GetDescendants()) do
-    if v.Name == 'Tag' then
-        v.Enabled = true
-            end
-        end
-    wait()
-    end)
-end
+local Section2 = Tab1:CreateSection("Misc")
 
-if tog == false then
-runServ:UnbindFromRenderStep("test")
-    end
-end)
-
-
-
-
-Section1:CreateButton("Auto Build Forts",function()
-   local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
-
-for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
-local ohTable1 = {
-[1] = v
-}
-local ohString2 = "Fortifications"
-workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
-    end 
-end)
-
-
-Section1:CreateDropdown("Build Mines on City with Selected Resource", {"Aluminum","Oil","Phosphate","Diamond","Chromium","Uranium","Iron","Copper","Titanium","Tungsten","Gold"}, function(state)
-    local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
-local resource = state
-
-for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
-  if v.Resources:FindFirstChild(resource) then
-      print(v)
-
-local ohTable1 = {
-[1] = v
-}
-local ohString2 = "Mines"
-
-workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
-end
-end
-end)
-
-
-Section1:CreateButton("Build Mines on Cities With Resources",function()
-    local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
-
-for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
-    print(v)
-  if v.Resources:FindFirstChildOfClass("Vector3Value") then
-      print(v)
-
-local ohTable1 = {
-[1] = v
-}
-local ohString2 = "Mines"
-
-workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
-print(v)
-end
-end
-end)
-
-Section1:CreateButton("Send Alliance to All Players",function()
-     for i,v in pairs(workspace.CountryData:GetChildren()) do
-                if not string.match(v.Leader.Value, "AI") and not string.match(v.Name, game.Players.LocalPlayer.Name) then
-                    wait(0.7)
-                    workspace.GameManager.ManageAlliance:FireServer(v.Name,"SendRequest")
-                end
-                for i,v in pairs(game.Players.LocalPlayer.PlayerGui.GameGui.MainFrame:GetChildren()) do
-                    if v.Name == "AlertSample" then
-                        v:Destroy()
-                    end
-                end
-            end	    
-end)
-
-
-Section1:CreateButton("Send Puppet to All Players",function()
-     for i,v in pairs(workspace.CountryData:GetChildren()) do
-                if not string.match(v.Leader.Value, "AI") and not string.match(v.Name, game.Players.LocalPlayer.Name) then
-                    wait(0.7)
-                    workspace.GameManager.ManageAlliance:FireServer(v.Name,"SendRequest","PuppetRequest")
-                end
-                for i,v in pairs(game.Players.LocalPlayer.PlayerGui.GameGui.MainFrame:GetChildren()) do
-                    if v.Name == "AlertSample" then
-                        v:Destroy()
-                    end
-                end
-            end	    
-    end)
-
-
-
-local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
-local resource = v 
-
-for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
-  if v.Resources:FindFirstChild(resource) then
-      print(v)
-
-local ohTable1 = {
-[1] = v
-}
-local ohString2 = "Mines"
-
-workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
-end
-end
-
-
-
-
-Section1:CreateButton("Country Lookup",function()
-  local  Country = Instance.new("ScreenGui")
+Section2:CreateButton("Country Lookup",function()
+  local xSpy2Country = Instance.new("ScreenGui")
             local Country = Instance.new("Frame")
             local TextLabel = Instance.new("TextLabel")
             local Search = Instance.new("TextBox")
@@ -165,12 +44,12 @@ Section1:CreateButton("Country Lookup",function()
             local TextLabel_2 = Instance.new("TextLabel")
             local WE = Instance.new("TextLabel")
             local Close = Instance.new("TextButton")
-             Country.Name = " Country"
-             Country.Parent = game.CoreGui
-             Country.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+            xSpy2Country.Name = "xSpy2Country"
+            xSpy2Country.Parent = game.CoreGui
+            xSpy2Country.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
             
             Country.Name = "Country"
-            Country.Parent =  Country
+            Country.Parent = xSpy2Country
             Country.Active = true
             Country.BackgroundColor3 = Color3.new(0.113725, 0.113725, 0.113725)
             Country.BorderSizePixel = 0
@@ -515,5 +394,130 @@ Section1:CreateButton("Country Lookup",function()
         WE.Text = "War Exhaustion: "..countryData[thisCountry].Power.WarExhaustion.Value
         AT.Text = "Amount in Treasury: $"..countryData[thisCountry].Economy.Balance.Value
     end)
-
 end)
+
+
+
+Section1:CreateToggle("Unit Esp | Kinda Laggy",nil,function(value)
+    tog = value
+    local runServ = game:GetService("RunService")
+if tog == true then
+runServ:BindToRenderStep("test", Enum.RenderPriority.Camera.Value, function() 
+for i,v in pairs(game:GetService("Workspace").Units:GetDescendants()) do
+    if v.Name == 'Tag' then
+        v.Enabled = true
+            end
+        end
+    wait()
+    end)
+end
+
+if tog == false then
+runServ:UnbindFromRenderStep("test")
+    end
+end)
+
+
+
+
+Section1:CreateButton("Auto Build Forts",function()
+   local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
+
+for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
+local ohTable1 = {
+[1] = v
+}
+local ohString2 = "Fortifications"
+workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
+    end 
+end)
+
+
+Section1:CreateDropdown("Build Mines on City with Selected Resource", {"Aluminum","Oil","Phosphate","Diamond","Chromium","Uranium","Iron","Copper","Titanium","Tungsten","Gold"}, function(state)
+    local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
+local resource = state
+
+for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
+  if v.Resources:FindFirstChild(resource) then
+      print(v)
+
+local ohTable1 = {
+[1] = v
+}
+local ohString2 = "Mines"
+
+workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
+end
+end
+end)
+
+
+Section1:CreateButton("Build Mines on Cities With Resources",function()
+    local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
+
+for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
+    print(v)
+  if v.Resources:FindFirstChildOfClass("Vector3Value") then
+      print(v)
+
+local ohTable1 = {
+[1] = v
+}
+local ohString2 = "Mines"
+
+workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
+print(v)
+end
+end
+end)
+
+Section1:CreateButton("Send Alliance to All Players",function()
+     for i,v in pairs(workspace.CountryData:GetChildren()) do
+                if not string.match(v.Leader.Value, "AI") and not string.match(v.Name, game.Players.LocalPlayer.Name) then
+                    wait(0.7)
+                    workspace.GameManager.ManageAlliance:FireServer(v.Name,"SendRequest")
+                end
+                for i,v in pairs(game.Players.LocalPlayer.PlayerGui.GameGui.MainFrame:GetChildren()) do
+                    if v.Name == "AlertSample" then
+                        v:Destroy()
+                    end
+                end
+            end	    
+end)
+
+
+Section1:CreateButton("Send Puppet to All Players",function()
+     for i,v in pairs(workspace.CountryData:GetChildren()) do
+                if not string.match(v.Leader.Value, "AI") and not string.match(v.Name, game.Players.LocalPlayer.Name) then
+                    wait(0.7)
+                    workspace.GameManager.ManageAlliance:FireServer(v.Name,"SendRequest","PuppetRequest")
+                end
+                for i,v in pairs(game.Players.LocalPlayer.PlayerGui.GameGui.MainFrame:GetChildren()) do
+                    if v.Name == "AlertSample" then
+                        v:Destroy()
+                    end
+                end
+            end	    
+    end)
+
+
+
+local country = game:GetService("Players").LocalPlayer.leaderstats.Country.Value
+local resource = v 
+
+for i,v in pairs(game:GetService("Workspace").Baseplate.Cities[country]:GetChildren()) do
+  if v.Resources:FindFirstChild(resource) then
+      print(v)
+
+local ohTable1 = {
+[1] = v
+}
+local ohString2 = "Mines"
+
+workspace.GameManager.CreateBuilding:FireServer(ohTable1, ohString2)
+end
+end
+
+
+
+
